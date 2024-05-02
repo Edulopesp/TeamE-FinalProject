cda5905098507541b9b127e6fa915c6aec64547c
Eduardo Lopes <129979638+Edulopesp@users.noreply.github.com>
2024-05-03 00:58:00 +0100

    .

--- TeamE-Project/Areas/Identity/Data/DataContext.cs
+++ TeamE-Project/Areas/Identity/Data/DataContext.cs
@@ -12,6 +12,14 @@ public class DataContext : IdentityDbContext<IdentityUser>
     {
     }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            "Server=TeamE-Project;Database=teame_db;User=root;Password=yourpassword;",
+            new MySqlServerVersion(new Version(8, 0, 22))
+        );
+    }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
---
+++ TeamE-Project/Views/Shared/_Sidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeamE_Project.Views.Shared
+{
+    public class _SidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
